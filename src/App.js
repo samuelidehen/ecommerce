b01fec84921fd75860fedfe6daaef42664961cb9
Samuel Idehen <samuelidehenlion@gmail.com>
2020-04-13 16:01:40 -0400

    --header,shopPage,HomePage build

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/HomePage/HomePage";
+import ShopPage from "./pages/ShopPage/ShopPage";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
