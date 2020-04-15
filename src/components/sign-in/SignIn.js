a9b97aabaa849584cc9d4277cb579b578f321ce4
Samuel Idehen <samuelidehenlion@gmail.com>
2020-04-15 01:45:49 -0400

    finishing sign-in

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../inputs/form-input/FormInput";
 import CustomButton from "../inputs/custom-button/CustomButton";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./SignIn.scss";
 
@@ -13,10 +13,17 @@ class SignIn extends React.Component {
     password: ""
   };
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = event => {
