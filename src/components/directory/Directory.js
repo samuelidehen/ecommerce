1e842322478dde64d4aa2c02935e854da54330e1
Samuel Idehen <samuelidehenlion@gmail.com>
2020-04-17 13:49:28 -0400

    --directory redux setup

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -1,54 +1,18 @@
 import React from "react";
-
+import { connect } from "react-redux";
 import MenuItem from "../menu-item/MenuItem";
 
 import "./Directory.scss";
 
-class Directory extends React.Component {
-  state = {
-    sections: [
-      {
-        title: "hats",
-        imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-        id: 1,
-        linkUrl: "hats"
-      },
-      {
-        title: "jackets",
-        imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-        id: 2,
-        linkUrl: ""
-      },
-      {
-        title: "sneakers",
-        imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-        id: 3,
-        linkUrl: ""
-      },
-      {
-        title: "womens",
-        imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-        size: "large",
-        id: 4
-      },
-      {
-        title: "mens",
-        imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-        size: "large",
-        id: 5
-      }
-    ]
-  };
+const Directory = ({ sections }) => {
+  return (
+    <div className="directory-menu">
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
+};
+const mapStateToProps = ({ directory }) => ({ sections: directory.sections });
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
